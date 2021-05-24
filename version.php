0606d03a929fc05afbe2905fdb99dee838baf93b
Adrian Perez <me@adrianperez.me>
2021-05-24 21:05:59 +0200

    Bump version.php and update moodle-ci.yml

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020111400;
-$plugin->requires = 2020110900;
+$plugin->version  = 2021052400;
+$plugin->requires = 2021051700;
 $plugin->component = 'local_pluginsfetcher';
-$plugin->release = 'v3.10-r1';
+$plugin->release = 'v3.11-r1';
 $plugin->maturity = MATURITY_STABLE;
